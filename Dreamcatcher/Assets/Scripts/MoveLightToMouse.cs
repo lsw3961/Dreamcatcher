1c426f6cccc682c183e70f652124ba43566be2e3
Logan White <lsw3961@ad.rit.edu>
2020-10-24 14:05:40 -0400

    anotoa
    anotha

---
+++ Dreamcatcher/Assets/Scripts/MoveLightToMouse.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLightToMouse : MonoBehaviour
+{
+    /// <summary>
+    /// this update is just going to set the position of the player light to
+    /// the mouse position
+    /// </summary>
+    public GameObject PlayerLight;
+    Vector2 screenPosition;
+    Vector2 worldPosition;
+    void FixedUpdate()
+    {
+        screenPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+        PlayerLight.transform.position = worldPosition;
+    }
+}

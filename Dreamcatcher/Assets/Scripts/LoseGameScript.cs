396b27ace185cc6422da91ebd86c6f5465587362
Logan White <lsw3961@ad.rit.edu>
2020-10-24 21:19:39 -0400

    morestuff
    morestuff

---
+++ Dreamcatcher/Assets/Scripts/LoseGameScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseGameScript : MonoBehaviour
+{
+    public int health;
+    public ChangeScene changeSceneScript;
+    public int sceneChange;
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(health);
+        health--;
+        if (health <= 0)
+        {
+            changeSceneScript.MoveScenes(sceneChange);
+        }
+    }
+}

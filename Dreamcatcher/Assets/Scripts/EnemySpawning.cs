2010310aea15429ce878e41e1dfd555f0d89eb1d
Logan White <lsw3961@ad.rit.edu>
2020-10-26 02:18:38 -0400

    final touches
    we almost did it boys, we just about have the full game

--- Dreamcatcher/Assets/Scripts/EnemySpawning.cs
+++ Dreamcatcher/Assets/Scripts/EnemySpawning.cs
@@ -19,30 +19,57 @@ public class EnemySpawning : MonoBehaviour
         width = height * cam.aspect;
         for (int i = 0; i < numberOfGhosts; i++)
         {
-            int positionDecider = Random.Range(1, 4);
+            int positionDecider = Random.Range(1, 8);
             switch (positionDecider)
             {
-                    //top
+                    //top right
                 case (1):
-                    position = new Vector3(Random.Range(-10f, 10f), Random.Range(5,5.5f));
+                    position = new Vector3(Random.Range(0, 10f), Random.Range(5,5.5f));
+                    enemies.Add(Instantiate(prefabs[Random.Range(0, prefabs.Count)], position, Quaternion.identity));
                     break;
-                    //right
+                    //right top
                 case (2):
-                    position = new Vector3(Random.Range(10f, 10.5f), Random.Range(-5f, 5f));
+                    position = new Vector3(Random.Range(10f, 10.5f), Random.Range(0, 5f));
+                    enemies.Add(Instantiate(prefabs[Random.Range(0, prefabs.Count)], position, Quaternion.identity));
                     break;
-                    //bottom
+                    //bottom right
                 case (3):
-                    position = new Vector3(Random.Range(-10f, 10f), Random.Range(-5.5f, -5f));
+                    position = new Vector3(Random.Range(0, 10f), Random.Range(-5.5f, -5f));
+                    enemies.Add(Instantiate(prefabs[Random.Range(0, prefabs.Count)], position, Quaternion.identity));
+                    break;
+                    //left top
+                case (4):
+                    position = new Vector3(Random.Range(-10f, -10.5f), Random.Range(0, 5f));
+                    enemies.Add(Instantiate(prefabs[Random.Range(0, prefabs.Count)], position, Quaternion.identity));
+                    enemies[i].GetComponent<SpriteRenderer>().flipX = true;
+                    break;
+                //top left
+                case (5):
+                    position = new Vector3(Random.Range(-10f,0), Random.Range(5, 5.5f));
+                    enemies.Add(Instantiate(prefabs[Random.Range(0, prefabs.Count)], position, Quaternion.identity));
+                    enemies[i].GetComponent<SpriteRenderer>().flipX = true;
+                    break;
+                //right bottom
+                case (6):
+                    position = new Vector3(Random.Range(10f, 10.5f), Random.Range(-5.5f, 0));
+                    enemies.Add(Instantiate(prefabs[Random.Range(0, prefabs.Count)], position, Quaternion.identity));
+                    break;
+                //bottom left
+                case (7):
+                    position = new Vector3(Random.Range(-10f, 0f), Random.Range(-5.5f, -5f));
+                    enemies.Add(Instantiate(prefabs[Random.Range(0, prefabs.Count)], position, Quaternion.identity));
+                    enemies[i].GetComponent<SpriteRenderer>().flipX = true;
 
                     break;
-                    //left
-                case (4):
-                    position = new Vector3(Random.Range(10f, 10.5f), Random.Range(-5.5f, -5f));
-
+                //left bottom
+                case (8):
+                    position = new Vector3(Random.Range(-10f, -10.5f), Random.Range(-5.5f,0));
+                    enemies.Add(Instantiate(prefabs[Random.Range(0, prefabs.Count)], position, Quaternion.identity));
+                    enemies[i].GetComponent<SpriteRenderer>().flipX = true;
                     break;
             }
             
-            enemies.Add(Instantiate(prefabs[Random.Range(0, prefabs.Count)], position, Quaternion.identity));
+
         }
         //Debug.Log(enemies.Count);
     }
@@ -50,29 +77,60 @@ public class EnemySpawning : MonoBehaviour
     {
         enemies.Remove(removed);
         scoreKeeper.updateScore();
-        int positionDecider = Random.Range(1, 4);
+        int positionDecider = Random.Range(1, 8);
         switch (positionDecider)
         {
-            //top
+            //top right
             case (1):
-                position = new Vector3(Random.Range(-10f, 10f), Random.Range(5.5f, 5f));
+                position = new Vector3(Random.Range(0, 10f), Random.Range(5, 5.5f));
+                enemies.Add(Instantiate(prefabs[Random.Range(0, prefabs.Count)], position, Quaternion.identity));
                 break;
-            //right
+            //right top
             case (2):
-                position = new Vector3(Random.Range(10f, 10.5f), Random.Range(5f, 5.5f));
+                position = new Vector3(Random.Range(10f, 10.5f), Random.Range(0, 5f));
+                enemies.Add(Instantiate(prefabs[Random.Range(0, prefabs.Count)], position, Quaternion.identity));
                 break;
-            //bottom
+            //bottom right
             case (3):
-                position = new Vector3(Random.Range(-10f, 10f), Random.Range(-5.5f, -5f));
+                position = new Vector3(Random.Range(0, 10f), Random.Range(-5.5f, -5f));
+                enemies.Add(Instantiate(prefabs[Random.Range(0, prefabs.Count)], position, Quaternion.identity));
+                break;
+            //left top
+            case (4):
+                position = new Vector3(Random.Range(-10f, -10.5f), Random.Range(0, 5f));
+                enemies.Add(Instantiate(prefabs[Random.Range(0, prefabs.Count)], position, Quaternion.identity));
+                enemies[enemies.Count - 1].GetComponent<SpriteRenderer>().flipX = true;
+                break;
+            //top left
+            case (5):
+                position = new Vector3(Random.Range(-10f, 0), Random.Range(5, 5.5f));
+                enemies.Add(Instantiate(prefabs[Random.Range(0, prefabs.Count)], position, Quaternion.identity));
+                enemies[enemies.Count - 1].GetComponent<SpriteRenderer>().flipX = true;
+                break;
+            //right bottom
+            case (6):
+                position = new Vector3(Random.Range(10f, 10.5f), Random.Range(-5.5f, 0));
+                enemies.Add(Instantiate(prefabs[Random.Range(0, prefabs.Count)], position, Quaternion.identity));
+                break;
+            //bottom left
+            case (7):
+                position = new Vector3(Random.Range(-10f, 0f), Random.Range(-5.5f, -5f));
+                enemies.Add(Instantiate(prefabs[Random.Range(0, prefabs.Count)], position, Quaternion.identity));
+                enemies[enemies.Count - 1].GetComponent<SpriteRenderer>().flipX = true;
 
                 break;
-            //left
-            case (4):
-                position = new Vector3(Random.Range(10f, 10.5f), Random.Range(-5.5f, -5f));
-
+            //left bottom
+            case (8):
+                position = new Vector3(Random.Range(-10f, -10.5f), Random.Range(-5.5f, 0));
+                enemies.Add(Instantiate(prefabs[Random.Range(0, prefabs.Count)], position, Quaternion.identity));
+                enemies[enemies.Count - 1].GetComponent<SpriteRenderer>().flipX = true;
                 break;
         }
-        enemies.Add(Instantiate(prefabs[Random.Range(0, prefabs.Count)], position, Quaternion.identity));
     }
 
 }
+
+
+
+
+//enemies[enemies.Count - 1].GetComponent<SpriteRenderer>().flipY = true;

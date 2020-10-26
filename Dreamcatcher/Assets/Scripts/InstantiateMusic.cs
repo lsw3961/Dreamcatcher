4c7924b3ec6ccbc21c25d230102617da60826925
mmc9164 <70117876+mmc9164@users.noreply.github.com>
2020-10-26 01:10:24 -0400

    Working On Music

---
+++ Dreamcatcher/Assets/Scripts/InstantiateMusic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateMusic : MonoBehaviour
+{
+    [SerializeField]
+    GameObject musicPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(musicPlayer);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

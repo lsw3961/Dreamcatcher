12d5176f0b666adb519c34acc579b724a0013ea8
Logan White <lsw3961@ad.rit.edu>
2020-10-25 23:33:34 -0400

    stuff
    stuffer

---
+++ Dreamcatcher/Assets/Scripts/DisplayTImer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DisplayTImer : MonoBehaviour
+{
+    float temp;
+    float hours;
+    float minutes;
+    public Text timeText;
+    string ampm;
+    void Start()
+    {
+        ampm = "pm";
+        hours = 8;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void DisplayTime(float timeToDisplay)
+    {
+        minutes = Mathf.Floor(Mathf.Repeat(timeToDisplay / 5, 6));
+        Debug.Log("time: " + (timeToDisplay));
+        Debug.Log("time Modulo: " + (timeToDisplay % 30));
+        if ((timeToDisplay % 30) <= .003999f)
+        {
+            hours++;
+           
+        }
+        //hours += temp;
+        if (hours>12)
+        { 
+             ampm = "am";
+            hours = 0;
+        }
+        else
+        {
+             ampm = "pm";
+        }
+        timeText.text = string.Format("{0:00}:{1:0}0 {2}", hours, minutes, ampm);
+    }
+}

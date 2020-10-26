9eded08283690c77846f031d49ae377ccae83217
Logan White <lsw3961@ad.rit.edu>
2020-10-26 00:16:39 -0400

    Matt do your stuff
    options added so is timer

--- Dreamcatcher/Assets/Scripts/DisplayTImer.cs
+++ Dreamcatcher/Assets/Scripts/DisplayTImer.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEngine.UI;
 public class DisplayTImer : MonoBehaviour
 {
+    float moduloTime;
+    public bool workAround;
     float temp;
     float hours;
     float minutes;
@@ -13,6 +15,7 @@ public class DisplayTImer : MonoBehaviour
     {
         ampm = "pm";
         hours = 8;
+        moduloTime = 5;
     }
 
     void Update()
@@ -24,17 +27,22 @@ public class DisplayTImer : MonoBehaviour
     {
         minutes = Mathf.Floor(Mathf.Repeat(timeToDisplay / 5, 6));
         Debug.Log("time: " + (timeToDisplay));
-        Debug.Log("time Modulo: " + (timeToDisplay % 30));
-        if ((timeToDisplay % 30) <= .003999f)
+        Debug.Log("time Modulo: " + ((timeToDisplay % 3) + 2));
+
+        moduloTime -= Time.deltaTime;
+        
+        if (Mathf.FloorToInt(timeToDisplay % 30) < 1 &&moduloTime<0)
         {
             hours++;
-           
+            moduloTime = 5;
         }
-        //hours += temp;
+        if (hours == 12)
+        {
+            ampm = "am";
+        }
         if (hours>12)
         { 
-             ampm = "am";
-            hours = 0;
+            hours = 1;
         }
         else
         {

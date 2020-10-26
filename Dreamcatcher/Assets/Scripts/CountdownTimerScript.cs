87753794da261b56cac5ab5d8a84b0bcbee2a567
Logan White <lsw3961@ad.rit.edu>
2020-10-26 01:09:16 -0400

    more shot
    pls god no errors

--- Dreamcatcher/Assets/Scripts/CountdownTimerScript.cs
+++ Dreamcatcher/Assets/Scripts/CountdownTimerScript.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 ﻿using System.Collections;
 using System.Collections.Generic;
@@ -73,3 +74,56 @@ public class CountdownTimerScript : MonoBehaviour
     }
 }
 >>>>>>> 9eded08283690c77846f031d49ae377ccae83217
+=======
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CountdownTimerScript : MonoBehaviour
+{
+    public SceneTransitionLoading changeScenes;
+    public static int dayNumber = 1;
+    public float timeUntilDawn = 270;
+    public float timeRemaining;
+    public bool runTimer;
+    public DisplayTImer timerDisplay;
+    public Text dayText;
+    // Update is called once per frame
+    public void Start()
+    {
+        runTimer = true;
+        timeRemaining = 0;
+        dayText.text = ("DAY: " + dayNumber);
+    }
+
+
+    void Update()
+    {
+        if (runTimer)
+        {
+            if (timeRemaining < timeUntilDawn)
+            {
+                timeRemaining += Time.deltaTime;
+            }
+            else
+            {
+                dayText.text = ("YOU SURVIVED THE NIGHT!");
+                runTimer = false;
+                timeRemaining = 0;
+                dayNumber++;
+                Debug.Log(dayNumber);
+                if (dayNumber != 8)
+                {
+                    changeScenes.LoadNextLevel(4);
+                }
+                else
+                {
+                    changeScenes.LoadNextLevel(6);
+                }
+
+            }
+        }
+        timerDisplay.DisplayTime(timeRemaining);
+    }
+}
+>>>>>>> Stashed changes
--- Dreamcatcher/Assets/Scripts/DisplayTImer.cs
+++ Dreamcatcher/Assets/Scripts/DisplayTImer.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 ﻿using System.Collections;
 using System.Collections.Generic;
@@ -107,3 +108,56 @@ public class DisplayTImer : MonoBehaviour
     }
 }
 >>>>>>> 9eded08283690c77846f031d49ae377ccae83217
+=======
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DisplayTImer : MonoBehaviour
+{
+    float moduloTime;
+    public bool workAround;
+    float temp;
+    float hours;
+    float minutes;
+    public Text timeText;
+    string ampm;
+    void Start()
+    {
+        ampm = "pm";
+        hours = 8;
+        moduloTime = 5;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void DisplayTime(float timeToDisplay)
+    {
+        minutes = Mathf.Floor(Mathf.Repeat(timeToDisplay / 5, 6));
+
+        moduloTime -= Time.deltaTime;
+        
+        if (Mathf.FloorToInt(timeToDisplay % 30) < 1 &&moduloTime<0)
+        {
+            hours++;
+            moduloTime = 5;
+        }
+        if (hours == 12)
+        {
+            ampm = "am";
+        }
+        if (hours>12)
+        { 
+            hours = 1;
+        }
+        else
+        {
+             ampm = "pm";
+        }
+        timeText.text = string.Format("{0:00}:{1:0}0 {2}", hours, minutes, ampm);
+    }
+}
+>>>>>>> Stashed changes
